9dc9e291dde9e98a22d10ce9b7ae15ac4e93e729
DavidCarlyn <davidecarlyn@gmail.com>
2019-05-20 14:05:10 -0400

    Testing build of react app

---
+++ precache-manifest.d45d94f989c69df93a4d5456298add85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0012b04b3beb83a0a0a268efe387ffa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd573316073707593fe1",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "cd573316073707593fe1",
+    "url": "/static/js/main.5ad47af1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
